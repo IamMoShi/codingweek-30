1fb7035ce39c521e833ea274be2fd26cca5ad5d8
Léo Fornoff <leo.fornoff@telecomnancy.eu>
2024-01-08 17:27:18 +0100

    database creation AddressController

---
+++ condingLate/src/test/java/eu/telecomnancy/codinglate/database/controller/AddressTest.java
@@ -0,0 +1,56 @@
+package eu.telecomnancy.codinglate.database.controller;
+
+import eu.telecomnancy.codinglate.database.dataController.user.AddressController;
+import eu.telecomnancy.codinglate.database.dataObject.user.Address;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddressTest {
+    private AddressController addressController = new AddressController();
+
+    @Test
+    public void testInsertAddress() {
+        Address newAddress = new Address("123 Rue de la Test");
+        addressController.insert(newAddress);
+
+        int addressId = newAddress.getId();
+        Address retrievedAddress = addressController.getAddressById(addressId);
+
+        assertNotNull(retrievedAddress);
+        assertEquals("123 rue de la test", retrievedAddress.getAddress());
+    }
+
+    @Test
+    public void testUpdateAddress() {
+        Address newAddress = new Address("456 Avenue de Test");
+        addressController.insert(newAddress);
+
+        int addressId = newAddress.getId();
+        Address retrievedAddress = addressController.getAddressById(addressId);
+
+        assertNotNull(retrievedAddress);
+
+        retrievedAddress.setAddress("789 Boulevard de Test");
+        addressController.update(retrievedAddress);
+
+        Address updatedAddress = addressController.getAddressById(addressId);
+        assertEquals("789 boulevard de test", updatedAddress.getAddress());
+    }
+
+    @Test
+    public void testDeleteAddress() {
+        Address newAddress = new Address("987 Avenue de Test");
+        addressController.insert(newAddress);
+
+        int addressId = newAddress.getId();
+        Address retrievedAddress = addressController.getAddressById(addressId);
+
+        assertNotNull(retrievedAddress);
+
+        addressController.deleteAddressById(addressId);
+        Address deletedAddress = addressController.getAddressById(addressId);
+
+        assertNull(deletedAddress);
+    }
+}
