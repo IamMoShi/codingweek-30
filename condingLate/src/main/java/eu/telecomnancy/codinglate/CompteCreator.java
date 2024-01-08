7e50744cbe118b987501933488f747b1127ff437
FONDRAT Julie <julie.fondrat@telecomnancy.eu>
2024-01-08 14:09:34 +0000

    Update CompteCreator.java

--- condingLate/src/main/java/eu/telecomnancy/codinglate/CompteCreator.java
+++ condingLate/src/main/java/eu/telecomnancy/codinglate/CompteCreator.java
@@ -18,7 +18,7 @@ public class CompteCreator extends Application {
         GridPane gridPane = createFormPane();
         addUIControls(gridPane);
 
-        Scene scene = new Scene(gridPane, 400, 200);
+        Scene scene = new Scene(gridPane, 700, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -33,24 +33,58 @@ public class CompteCreator extends Application {
 
     private void addUIControls(GridPane gridPane) {
         // Add controls to the gridPane
-        Label nameLabel = new Label("Name:");
-        TextField nameField = new TextField();
-        gridPane.add(nameLabel, 0, 0);
-        gridPane.add(nameField, 1, 0);
+
+        Label idLabel = new Label("ID:");
+        TextField idField = new TextField();
+        gridPane.add(idLabel, 0, 0);
+        gridPane.add(idField, 1, 0);
+
+
+        Label FirstnameLabel = new Label("FirstName:");
+        TextField FirstnameField = new TextField();
+        gridPane.add(FirstnameLabel, 0, 1);
+        gridPane.add(FirstnameField, 1, 1);
+
+        Label LastnameLabel = new Label("LastName:");
+        TextField LastnameField = new TextField();
+        gridPane.add(LastnameLabel, 0, 2);
+        gridPane.add(LastnameField, 1, 2);
+
+        Label emailLabel = new Label("emailName:");
+        TextField emailField = new TextField();
+        gridPane.add(emailLabel, 0, 3);
+        gridPane.add(emailField, 1, 3);
+
+        Label phoneLabel = new Label("phone:");
+        TextField phoneField = new TextField();
+        gridPane.add(phoneLabel, 0, 4);
+        gridPane.add(phoneField, 1, 4);
+
+        Label AdressLabel = new Label("Adress:");
+        TextField AdressField = new TextField();
+        gridPane.add(AdressLabel, 0, 5);
+        gridPane.add(AdressField, 1, 5);
+
 
         Label passwordLabel = new Label("Password:");
         TextField passwordField = new TextField();
-        gridPane.add(passwordLabel, 0, 1);
-        gridPane.add(passwordField, 1, 1);
+        gridPane.add(passwordLabel, 0, 6);
+        gridPane.add(passwordField, 1, 6);
+
 
         Button submitButton = new Button("Submit");
-        gridPane.add(submitButton, 0, 2, 2, 1);
+        gridPane.add(submitButton, 0, 6, 1, 7);
 
         // Event handling for the submit button
         submitButton.setOnAction(e -> {
-            String name = nameField.getText();
-            String email = passwordField.getText();
-            
+            int  id = Integer.getInteger(idField.getText());
+            String firstName = FirstnameField.getText();
+            String lastName = LastnameField.getText();
+            String email = emailField.getText();
+            String phone = phoneField.getText();
+            String Adress = AdressField.getText();
+            String password = passwordField.getText();
+
         });
     }
 
@@ -58,6 +92,3 @@ public class CompteCreator extends Application {
         launch(args);
     }
 }
-
-
-
