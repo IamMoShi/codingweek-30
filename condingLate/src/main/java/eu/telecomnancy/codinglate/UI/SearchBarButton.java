d210f59b6a6adf8e84d6275905918e51baf30dec
neyen <elise.neyens@telecomnancy.eu>
2024-01-08 16:51:56 +0100

    Merge et ajout form

--- condingLate/src/main/java/eu/telecomnancy/codinglate/UI/SearchBarButton.java
+++ condingLate/src/main/java/eu/telecomnancy/codinglate/UI/SearchBarButton.java
@@ -2,11 +2,10 @@ package eu.telecomnancy.codinglate.UI;
 
 public class SearchBarButton extends AbstractButton{
     public SearchBarButton(String buttonName, String Label) {
-        super(buttonName, Label);
+        super(buttonName, Label, null);
     }
 
     @Override
     protected void handleAction() {
-
     }
 }
