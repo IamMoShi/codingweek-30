513cde744e68dc0b9b2e13b3bb10666411055162
Léo Fornoff <leo.fornoff@telecomnancy.eu>
2024-01-11 15:57:56 +0100

    Calendrier pour les offres

--- condingLate/src/main/java/eu/telecomnancy/codinglate/App.java
+++ condingLate/src/main/java/eu/telecomnancy/codinglate/App.java
@@ -1,62 +1,37 @@
 package eu.telecomnancy.codinglate;
 
 
-import com.calendarfx.model.Calendar;
-import com.calendarfx.model.CalendarSource;
-import com.calendarfx.model.Resource;
-import com.calendarfx.view.CalendarView;
-import com.calendarfx.view.ResourcesView;
-import com.calendarfx.view.WeekView;
-import eu.telecomnancy.codinglate.calendar.CalendarStaticContraint;
-import eu.telecomnancy.codinglate.database.dataController.offer.OfferController;
 import eu.telecomnancy.codinglate.database.dataController.user.PersonController;
-import eu.telecomnancy.codinglate.database.dataObject.offer.Offer;
 import eu.telecomnancy.codinglate.database.dataObject.user.Address;
 import eu.telecomnancy.codinglate.database.dataObject.user.User;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class App extends Application {
 
     private SceneManager sceneManager;
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Offer offer = new OfferController().getOfferById(1);
-        Calendar calendar = new CalendarStaticContraint("Bookings", offer);
-
-        CalendarView calendarView = new CalendarView();
-        calendarView.setShowDeveloperConsole(false);
-        calendarView.setShowPageSwitcher(false);
-        calendarView.setShowSearchField(false);
-        calendarView.setShowAddCalendarButton(false);
-        calendarView.setShowPageToolBarControls(false);
-        calendarView.setShowPrintButton(false);
-        calendarView.setShowToolBar(false);
+    public void start(Stage stage) throws IOException {
 
 
-        CalendarSource myCalendarSource = new CalendarSource("My Calendars");
-        myCalendarSource.getCalendars().add(calendar);
-        calendarView.getCalendarSources().addAll(myCalendarSource);
+        Image appIcon = new Image(getClass().getResourceAsStream("/eu/telecomnancy/codinglate/icon/logo_hands.png"));
+        stage.getIcons().add(appIcon);
 
-        System.out.println(calendarView.getCalendars());
-        Calendar defaultCalendar = calendarView.getCalendars().get(0);
-        defaultCalendar.setName("CodingLate");
-        defaultCalendar.setStyle(Calendar.Style.STYLE2);
+        this.sceneManager = new SceneManager(stage);
 
+        Scene scene = sceneManager.createScenePresentation();
+        sceneManager.switchScene(scene);
 
-
-
-        Scene scene = new Scene(calendarView);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("CodingLate");
-        primaryStage.show();
+        User user =(User) PersonController.getInstance().getPersonByEmail("neyenselise@gmail.com");
+        if (user == null){
+            user = new User("Elise","neyens","neyenselise@gmail.com","password",new Address("1 rue de la paix"));
+            PersonController.getInstance().insert(user);
+        }
+        PersonController.getInstance().setCurrentUser(user);
     }
 
     public static void main(String[] args) {
---
+++ condingLate/src/main/java/eu/telecomnancy/codinglate/calendar/ReservationCalendarView.java
@@ -0,0 +1,67 @@
+package eu.telecomnancy.codinglate.calendar;
+
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.model.Entry;
+import com.calendarfx.model.Resource;
+import com.calendarfx.view.CalendarView;
+import com.calendarfx.view.ResourcesView;
+import com.calendarfx.view.WeekView;
+
+import eu.telecomnancy.codinglate.database.dataController.user.PersonController;
+import eu.telecomnancy.codinglate.database.dataObject.offer.Booking;
+import eu.telecomnancy.codinglate.database.dataObject.offer.Offer;
+import eu.telecomnancy.codinglate.database.dataObject.user.User;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ReservationCalendarView extends CalendarView {
+
+    private Calendar defaultCalendar;
+    private Offer offer;
+
+    public ReservationCalendarView(Offer offer) {
+
+        this.offer = offer;
+
+        this.setShowDeveloperConsole(false);
+        this.setShowPageSwitcher(false);
+        this.setShowSearchField(false);
+        this.setShowAddCalendarButton(false);
+        this.setShowPageToolBarControls(false);
+        this.setShowPrintButton(false);
+        this.setShowToolBar(false);
+
+        Calendar calendar = new CalendarStaticContraint("Bookings", offer);
+
+        defaultCalendar = new Calendar("default");
+        defaultCalendar.setStyle(Calendar.Style.STYLE1);
+
+        CalendarSource myCalendarSource = new CalendarSource("My Calendars");
+        myCalendarSource.getCalendars().add(calendar);
+        myCalendarSource.getCalendars().add(defaultCalendar);
+
+        this.getCalendarSources().addAll(myCalendarSource);
+
+
+    }
+
+    public Booking getBooking() {
+        List<Entry> entries = defaultCalendar.findEntries("");
+        // Regarde s'il y a bien une et une seule entrée dans le calendrier
+        if (entries.size() == 1) {
+            Entry entry = entries.get(0);
+            // Regarde si l'entrée est bien une réservation
+            if (entry.getUserObject() instanceof Booking) {
+                LocalDateTime startingDate = entry.getStartAsLocalDateTime();
+                LocalDateTime endingDate = entry.getEndAsLocalDateTime();
+                return new Booking(offer, (User) PersonController.getInstance().getCurrentUser(), startingDate, endingDate);
+            }
+        }
+        return null;
+    }
+}
